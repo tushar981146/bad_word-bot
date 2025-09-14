1c275d3d68d26eef6b1b001e141cd40ea61759d3
tushar981146 <tusharblackhathacker@gmail.com>
2025-09-14 19:16:39 +0530

    username fixed

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ bot.on('message', async (msg) => {
         const text = `owner ${owner},  ${msg.from.username}  is making trouble in this group`
         if (checker.check(input)) {
 
-            if(owner === msg.chat.username) {
+            if(owner === msg.from.username) {
                 await bot.sendMessage(
                 ownerId, `${ownerId} please be calm`, {
                 reply_to_message_id: msg.message_id
